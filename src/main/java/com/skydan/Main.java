4ebc9dfc0b99b6748a2a89ab96f20e718657353a
skydan11 <oskidan8684@gmail.com>
2023-03-10 13:03:33 +0100

    added first GET method

--- src/main/java/com/skydan/Main.java
+++ src/main/java/com/skydan/Main.java
@@ -2,10 +2,26 @@ package com.skydan;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @SpringBootApplication
+@RestController
 public class Main {
     public static void main(String[] args) {
         SpringApplication.run(Main.class, args);
     }
+
+    @GetMapping("/")
+    public GreetResponce greet(){
+        GreetResponce responce = new GreetResponce(
+                "Hello",
+                List.of("Java", "JS"),
+                new Person("Oleg", 36));
+        return responce;
+    }
+    record Person(String name, int age){}
+    record GreetResponce(String greet, List<String> favProgrammingLanguages, Person person){}
 }
